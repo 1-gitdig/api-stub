9c4428a6e227efac03bb52e3470876277738d459
Домиславский Михаил Михайлович <mikhail.domislavskiy@fuib.com>
2017-06-07 15:25:40 +0300

    *access file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'db.json')) 
 server.use(middlewares)
 server.use(router)
 server.listen( (process.env.PORT || 3000), () => {
